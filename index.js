1016992b9309279d631fe7c1be1bd322c3d3bbbb
Jakub Korupczynski <jakub.korupczynski@gmail.com>
2015-12-26 02:02:15 +0000

    Fix: When namespace was not created before used there was an error. Now before first template code to check if namespace is there has been added. If there is no namespace object it will be created.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ HandlebarsFilters.prototype.constructor = HandlebarsFilters;
 function HandlebarsFilters (tree, options) {
   if (!(this instanceof HandlebarsFilters))
   {
-      return new HandlebarsFilters(tree, options)
+      return new HandlebarsFilters(tree, options);
   }
 
   if (!('srcDir' in options)) {
@@ -24,6 +24,7 @@ function HandlebarsFilters (tree, options) {
   this.options.targetExtension = options.targetExtension || ['js'];
   this.options.srcDir = options.srcDir || null;
   this.options.namespace = options.namespace || 'Handlebars.templates';
+  this.prepareNamespace(this.options.namespace);
 
   // Set options necessary for the filter
   filterOptions = {
@@ -39,7 +40,11 @@ HandlebarsFilters.prototype.processString = function (string, srcFile) {
   try {
     var templateName = this.getTemplateName(srcFile);
     var precompiled = handlebars.precompile(string, this.options);
-    var output = this.options.namespace + ' = ' + this.options.namespace + ' || {};';
+    var output = "";
+    if (typeof this.namespaceCode !== "undefined" && this.namespaceCode !== null) {
+      output = output + this.namespaceCode;
+      this.namespaceCode = null;
+    }
     output += this.options.namespace + '[\'' + templateName + '\'] = Handlebars.template(' + precompiled + ');';
     return output;
   } catch (err) {
@@ -73,4 +78,29 @@ HandlebarsFilters.prototype.getTemplateName = function (srcFile) {
     }
 
     return templateName;
-}
+};
+
+/**
+ * Prepares the namespace objects.
+ *
+ * Because this script doesn't know how deep the napespace is and if all the necessary objects for the namespace exists
+ * it has to check all and try to add creation of all of those before fist template is compiled and added to the project.
+ * @param  {[string]} namespace for example: Some.Namespace.Templates
+ */
+HandlebarsFilters.prototype.prepareNamespace = function(namespace) {
+  var namespaceArray = namespace.split('.');
+  var currentNamespace = "";
+  var namespaceCode = "";
+  for (var i = 0; i < namespaceArray.length; i++) {
+    if (i !== 0) {
+      currentNamespace = currentNamespace + ".";
+    }
+    currentNamespace = currentNamespace + namespaceArray[i];
+    if (i === 0) {
+      namespaceCode = "if (typeof "+ currentNamespace + " === 'undefined') { var " + currentNamespace + " = {};}";
+    } else {
+      namespaceCode = namespaceCode + currentNamespace + " = " + currentNamespace + " || {};";
+    }
+  }
+  this.namespaceCode = namespaceCode;
+};
